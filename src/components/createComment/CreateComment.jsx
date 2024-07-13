f6a9cbfa32d1a6c0db04a221d29b2409db93b907
Pon Channarith <ponchannarith@gmail.com>
2024-07-13 17:08:38 +0700

    New_Update

--- src/components/createComment/CreateComment.jsx
+++ src/components/createComment/CreateComment.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { Label, Textarea } from "flowbite-react";
+import { FaCommentDots } from "react-icons/fa";
 const CreateComment = () => {
   return (
     <>
@@ -29,12 +30,15 @@ const CreateComment = () => {
   </div>
   <div className="w-[681px] h-[158px] p-5 left-[90px] top-[60px] absolute bg-white rounded-lg flex-col justify-start items-start gap-5 inline-flex">
     <div className="w-[92px] h-[26px] text-zinc-800 text-xl font-semibold font-suwannaphum">ចំណងជើង</div>
+
     <div className="w-[635px] h-[18px] text-zinc-500 text-xs font-normal font-suwannaphum">កំណត់ជាក់លាក់ ហើយស្រមៃថាអ្នកកំពុងសួរសំណួរទៅមនុស្សម្នាក់ទៀត។<br/></div>
-    <div className="w-[640px] h-[34px] relative rounded-[5px] border border-gray-200">
-      <div className="h-[34px] p-2.5 left-0 top-0 absolute justify-start items-center gap-2.5 inline-flex">
-        <div className="text-gray-200 text-xs font-normal font-['Poppins']">e.g. What are the benefits of using function in java? </div>
+    <div className="max-w-md w-[900px]">
+      <div className="mb-2 block">
+        <Label htmlFor="comment" value="Your message" />
       </div>
+      <Textarea id="comment" placeholder="Here is comment" required rows={1} />
     </div>
+
   </div>
   <div className="w-[1204px] h-[835px] px-10 pb-10 left-0 top-[233px] absolute flex-col justify-start items-start gap-[30px] inline-flex">
     <div className="w-[751px] h-[226px] relative">
@@ -50,15 +54,15 @@ const CreateComment = () => {
       <div className="w-[187.11px] h-6 left-[563.89px] top-[180px] absolute bg-white justify-start items-start gap-5 inline-flex">
         <div className="w-[67px] h-6 relative">
           <div className="w-6 h-6 left-0 top-0 absolute" />
-          <div className="w-[19px] h-3.5 left-[32px] top-[7px] absolute text-zinc-500 text-xs font-normal font-['Poppins']">120</div>
+          <div className="w-[19px] h-3.5 left-[32px] top-[7px] absolute text-zinc-500 text-xs font-normal font-suwannaphum">120</div>
         </div>
         <div className="w-[65px] h-6 relative">
           <div className="w-6 h-6 left-0 top-0 absolute" />
-          <div className="w-[19px] h-3.5 left-[32px] top-[7px] absolute text-zinc-500 text-xs font-normal font-['Poppins']">120</div>
+          <div className="w-[19px] h-3.5 left-[32px] top-[7px] absolute text-zinc-500 text-xs font-normal font-suwannaphum">120</div>
         </div>
         <div className="w-[39px] h-6 relative">
           <div className="w-[18px] h-[18px] left-0 top-[3px] absolute" />
-          <div className="w-[7px] h-[13px] left-[32px] top-[6px] absolute text-zinc-500 text-xs font-normal font-['Poppins']">3</div>
+          <div className="w-[7px] h-[13px] left-[32px] top-[6px] absolute text-zinc-500 text-xs font-normal font-suwannaphum">3</div>
         </div>
       </div>
     </div>
@@ -75,138 +79,30 @@ const CreateComment = () => {
       <div className="w-[187.11px] h-6 left-[563.89px] top-[180px] absolute bg-white justify-start items-start gap-5 inline-flex">
         <div className="w-[67px] h-6 relative">
           <div className="w-6 h-6 left-0 top-0 absolute" />
-          <div className="w-[19px] h-3.5 left-[32px] top-[7px] absolute text-zinc-500 text-xs font-normal font-['Poppins']">120</div>
+          <div className="w-[19px] h-3.5 left-[32px] top-[7px] absolute text-zinc-500 text-xs font-normal font-suwannaphum">120</div>
         </div>
         <div className="w-[65px] h-6 relative">
           <div className="w-6 h-6 left-0 top-0 absolute" />
-          <div className="w-[19px] h-3.5 left-[32px] top-[7px] absolute text-zinc-500 text-xs font-normal font-['Poppins']">120</div>
+          <div className="w-[19px] h-3.5 left-[32px] top-[7px] absolute text-zinc-500 text-xs font-normal font-suwannaphum">120</div>
         </div>
         <div className="w-[39px] h-6 relative">
           <div className="w-[18px] h-[18px] left-0 top-[3px] absolute" />
-          <div className="w-[7px] h-[13px] left-[32px] top-[6px] absolute text-zinc-500 text-xs font-normal font-['Poppins']">3</div>
+          <div className="w-[7px] h-[13px] left-[32px] top-[6px] absolute text-zinc-500 text-xs font-normal font-suwannaphum">3</div>
         </div>
       </div>
     </div>
-    <div className="text-blue-900 text-base font-normal font-['Poppins'] underline">See more</div>
+    <div className="text-blue-900 text-base font-normal font-suwannaphum underline">See more</div>
     <div className="w-[688px] h-[37px] relative">
       <div className="w-[179px] h-[26px] left-0 top-0 absolute">
         <div className="w-6 h-6 left-0 top-0 absolute" />
-        <div className="w-[147px] h-6 left-[32px] top-[2px] absolute">
-          <div className="left-0 top-[2px] absolute text-zinc-800 text-sm font-semibold font-['Poppins'] tracking-wide">Comment Section</div>
+        <div className="w-[147px] h-6 left-[32px] top-[2px] absolute ">
+        <FaCommentDots />
+          <div className="left-5  top-[0px] absolute text-zinc-800 text-sm font-semibold font-suwannaphum tracking-wide">Comment Section</div>
         </div>
       </div>
       <div className="w-[765.02px] h-[0px] left-[-0.01px] top-[34.52px] absolute border border-gray-200"></div>
     </div>
-    <div className="w-[798px] h-[150px] relative">
-      <div className="w-[34px] h-[30px] left-[813px] top-[119px] absolute" />
-      <div className="w-[53px] h-[38px] px-[15px] py-2 left-[698px] top-[119px] absolute bg-blue-900 rounded-[5px] justify-center items-center gap-2 inline-flex">
-        <div className="w-4 h-4 relative" />
-      </div>
-      <div className="w-[748px] h-[99px] left-[3px] top-[-0px] absolute">
-        <div className="w-[748px] h-10 p-2 left-0 top-0 absolute bg-white rounded-tl-[15px] rounded-tr-[15px] border-l border-r border-t border-gray-200 justify-start items-start gap-2 inline-flex">
-          <div className="border-gray-200 justify-start items-start gap-0.5 flex">
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-          </div>
-          <div className="border-gray-200 justify-start items-start gap-0.5 flex">
-            <div className="w-[139px] pl-2 pr-1 rounded justify-center items-center gap-0.5 flex">
-              <div className="text-neutral-800 text-sm font-normal font-['Noto Sans'] leading-tight">Normal text</div>
-              <div className="w-4 h-4 relative" />
-            </div>
-          </div>
-          <div className="border-gray-200 justify-start items-start gap-0.5 flex">
-            <div className="w-[50px] pl-2 pr-1 rounded justify-center items-center gap-0.5 flex">
-              <div className="w-5 h-5 relative" />
-              <div className="w-4 h-4 relative" />
-            </div>
-          </div>
-          <div className="border-gray-200 justify-start items-start gap-0.5 flex">
-            <div className="w-[50px] pl-2 pr-1 rounded justify-center items-center gap-0.5 flex">
-              <div className="w-5 h-5 relative">
-                <div className="w-5 h-5 left-0 top-0 absolute bg-neutral-800 rounded" />
-                <div className="w-5 h-5 left-0 top-0 absolute opacity-70 mix-blend-multiply rounded border border-neutral-800" />
-              </div>
-              <div className="w-4 h-4 relative" />
-            </div>
-          </div>
-          <div className="border-gray-200 justify-start items-start gap-0.5 flex">
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-          </div>
-          <div className="border-gray-200 justify-start items-start gap-0.5 flex">
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-          </div>
-          <div className="border-gray-200 justify-start items-start gap-0.5 flex">
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-            <div className="justify-start items-start flex">
-              <div className="w-7 px-1 rounded justify-center items-center gap-0.5 flex">
-                <div className="w-5 h-5 relative" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="w-[748px] h-[59px] left-0 top-[40px] absolute bg-white rounded-bl-[15px] rounded-br-[15px] shadow border-l border-r border-b border-gray-200 flex-col justify-start items-start inline-flex" />
-      </div>
-    </div>
+      {/* here is section for flowbite react */}
   </div>
 </div>
     </section>
