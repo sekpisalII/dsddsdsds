f0bc742c428f2033ac0cad8eefd7221469b876c9
pisalsek99 <pisalsek88@gmail.com>
2024-08-06 22:24:47 +0700

    Update Cute Cute

--- src/components/rpCrad/ReplyCard.jsx
+++ src/components/rpCrad/ReplyCard.jsx
@@ -35,18 +35,17 @@ const ReplyCard = ({ forumId }) => {
         comments.map((comment) => (
           <div
             key={comment.id}
-            className="mx-auto my-8 flex max-w-screen-sm rounded-xl border border-gray-100 p-4 text-left text-gray-600 shadow-lg sm:p-8"
+            className="mx-auto my-8 flex max-w-screen-sm rounded-xl border border-gray-100 p-4 text-left text-black shadow-lg sm:p-8"
           >
             <img
-              className=" block max-w-full text-left align-middle sm:h-16 sm:w-16 w-12 h-12 rounded-full mr-4 ml-5"
+              className="block max-w-full text-left align-middle sm:h-16 sm:w-16 w-12 h-12 rounded-full mr-4 ml-5"
               src="https://wallpapers.com/images/hd/smiling-close-up-oggy-and-the-cockroaches-71njhqoakbau7nbm.jpg"
               alt="Profile Picture"
             />
             <div className="w-full text-left">
-              <div className="mb-2 flex flex-col justify-between text-gray-600 sm:flex-row">
-                <h3 className="font-medium">{comment.author}</h3>
-                <div className="text-gray-500 ml-5">
-                  {" "}
+              <div className="mb-2 flex flex-col justify-between text-black sm:flex-row">
+                <h3 className="text-lg text-black">{comment.author}</h3>
+                <div className=" text-black ml-5">
                   {new Date(comment.created_at).toLocaleString("en-US", {
                     month: "short",
                     day: "numeric",
@@ -57,8 +56,8 @@ const ReplyCard = ({ forumId }) => {
                   })}
                 </div>
               </div>
-              <p className="text-sm">{comment.content}</p>
-              <div className="mt-5 flex items-center justify-between text-gray-600">
+              <p className="text-sm text-black">{comment.content}</p>
+              <div className="mt-5 flex items-center justify-between text-black">
                 <button className="cursor-pointer border py-2 px-8 text-center text-xs leading-tight transition-colors duration-150 ease-in-out hover:border-gray-500 rounded-lg">
                   Reply
                 </button>
