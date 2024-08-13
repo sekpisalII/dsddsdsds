34244d9c391b66c0a41ae52c9a5b96aeba9632cc
Pon Channarith <ponchannarith@gmail.com>
2024-08-13 11:22:20 +0700

    update About-us page

--- src/components/allAboutUs/AllAboutUs.jsx
+++ src/components/allAboutUs/AllAboutUs.jsx
@@ -111,7 +111,7 @@ const AllAboutUs = () => {
       <p className="text-gray-200 -mt-2 text-sm md:text-base lg:text-lg font-suwannaphum">
       Center of Science and Technology Advanced Development
       </p>
-      <ul className="flex justify-center space-x-6 mt-2">
+      <ul className="flex justify-center  mt-2">
         <li>
           <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="text-xl text-white hover:text-red-600 ">
             <FaFacebook />
@@ -145,7 +145,7 @@ const AllAboutUs = () => {
               <p className="text-gray-200 -mt-2 text-sm md:text-base lg:text-lg font-suwannaphum">
               Center of Science and Technology Advanced Development
               </p>
-              <ul className="flex justify-center space-x-6 mt-2">
+              <ul className="flex justify-center mt-2">
                 <li>
                   <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="text-xl text-white hover:text-red-600 ">
                     <FaFacebook />
@@ -189,7 +189,7 @@ const AllAboutUs = () => {
       <p className="text-gray-200 -mt-2 text-sm md:text-base lg:text-lg font-suwannaphum">
       Center of Science and Technology Advanced Development
       </p>
-      <ul className="flex justify-center space-x-6 mt-2">
+      <ul className="flex justify-center  mt-2">
         <li>
           <a href="https://www.facebook.com/profile.php?id=100028119094073&mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer" className="text-xl text-white hover:text-red-600 ">
             <FaFacebook />
@@ -222,7 +222,7 @@ const AllAboutUs = () => {
       <p className="text-gray-200 -mt-2 text-sm md:text-base lg:text-lg font-suwannaphum">
       Center of Science and Technology Advanced Development
       </p>
-      <ul className="flex justify-center space-x-6 mt-2">
+      <ul className="flex justify-center  mt-2">
         <li>
           <a href="https://www.facebook.com/profile.php?id=100053897863470&mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer" className="text-xl text-white hover:text-red-600 ">
             <FaFacebook />
@@ -254,7 +254,7 @@ const AllAboutUs = () => {
       <p className="text-gray-200 -mt-2 text-sm md:text-base lg:text-lg font-suwannaphum">
       Center of Science and Technology Advanced Development
       </p>
-      <ul className="flex justify-center space-x-6 mt-2">
+      <ul className="flex justify-center  mt-2">
         <li>
           <a href="https://www.facebook.com/HongSreyneang.2004?mibextid=JRoKGi" target="_blank" rel="noopener noreferrer" className="text-xl text-white hover:text-red-600 ">
             <FaFacebook />
@@ -289,7 +289,7 @@ const AllAboutUs = () => {
       <p className="text-gray-200 -mt-2 text-sm md:text-base lg:text-lg font-suwannaphum">
       Center of Science and Technology Advanced Development
       </p>
-      <ul className="flex justify-center space-x-6 mt-2">
+      <ul className="flex justify-center  mt-2">
         <li>
           <a href="https://www.facebook.com/van.sok.980?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer" className="text-xl text-white hover:text-red-600 ">
             <FaFacebook />
@@ -322,7 +322,7 @@ const AllAboutUs = () => {
       <p className="text-gray-200 -mt-2 text-sm md:text-base lg:text-lg font-suwannaphum">
       Center of Science and Technology Advanced Development
       </p>
-      <ul className="flex justify-center space-x-6 mt-2">
+      <ul className="flex justify-center  mt-2">
         <li>
           <a href="https://www.facebook.com/ka.nika.7737769?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer" className="text-xl text-white hover:text-red-600 ">
             <FaFacebook />
@@ -355,7 +355,7 @@ const AllAboutUs = () => {
       <p className="text-gray-200 -mt-2 text-sm md:text-base lg:text-lg font-suwannaphum">
       Center of Science and Technology Advanced Development
       </p>
-      <ul className="flex justify-center space-x-6 mt-2">
+      <ul className="flex justify-center  mt-2">
         <li>
           <a href="https://www.facebook.com/nam.ponleu?mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer" className="text-xl text-white hover:text-red-600 ">
             <FaFacebook />
@@ -388,7 +388,7 @@ const AllAboutUs = () => {
       <p className="text-gray-200 -mt-2 text-sm md:text-base lg:text-lg font-suwannaphum">
       Center of Science and Technology Advanced Development
       </p>
-      <ul className="flex justify-center space-x-6 mt-2">
+      <ul className="flex justify-center  mt-2">
         <li>
           <a href="https://www.facebook.com/mengeam.seng?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer" className="text-xl text-white hover:text-red-600 ">
             <FaFacebook />
@@ -421,7 +421,7 @@ const AllAboutUs = () => {
       <p className="text-gray-200 -mt-2 text-sm md:text-base lg:text-lg font-suwannaphum">
       Center of Science and Technology Advanced Development
       </p>
-      <ul className="flex justify-center space-x-6 mt-2">
+      <ul className="flex justify-center  mt-2">
         <li>
           <a href="https://www.facebook.com/Boen.thi.monster?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer" className="text-xl text-white hover:text-red-600 ">
             <FaFacebook />
@@ -453,7 +453,7 @@ const AllAboutUs = () => {
       <p className="text-gray-200 -mt-2 text-sm md:text-base lg:text-lg font-suwannaphum">
       Center of Science and Technology Advanced Development
       </p>
-      <ul className="flex justify-center space-x-6 mt-2">
+      <ul className="flex justify-center  mt-2">
         <li>
           <a href="https://web.facebook.com/?_rdc=1&_rdr" target="_blank" rel="noopener noreferrer" className="text-xl text-white hover:text-red-600 ">
             <FaFacebook />
